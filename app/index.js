dece3ffe4a821f50502fd93ee450dd092ca085fd
Emanuel Vecchio <vecchioemanuel@gmail.com>
2017-03-11 18:26:48 -0300

    # adding destructuring and rest spread operator plugins

--- app/index.js
+++ app/index.js
@@ -5,7 +5,14 @@ console.log('started');
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// ES6 destructuring sample
+const o1 = { a: 1 };
+const o2 = { b: 2 };
+const o3 = { ...o1, ...o2 };
+
+console.log(o3);
+
 ReactDOM.render(
-  <h1>rocks!</h1>,
-  document.getElementsByTagName('body')[0]
+  <h1>let's rock!</h1>,
+  document.getElementById('root')
 );
